8000ddac018892dad94a4ccabf62e1745ac73b30
mohanacad01 <mohanacad01@brightskills.com>
2024-01-05 17:31:22 +0530

    files have been added

---
+++ js/jsdemo.js
@@ -0,0 +1,29 @@
+//console.log ("welcome to js demo");
+
+console.log (10);
+
+console.log (.50);
+
+console.log ({fname:"mohana", gender:"female"});
+console.table ({fname:"mohana", gender:"female"});
+
+console.log ([10,20,30,40]);
+console.table ([10,20,30,40]);
+
+var a=10;
+let b=20;
+const c= 30;
+console.log (a);
+console.log (b);
+console.log (c);
+
+a=15;
+console.log(typeof a);
+
+b="mohana";
+console.log(typeof b);
+
+
+var v1=10;
+var v2=15;
+console.log(v1+v2);
