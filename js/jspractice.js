2f822ee017c35cf3d0269aa8b4900e72ce00b19a
mohanacad01 <mohanacad01@brightskills.com>
2024-01-20 18:01:19 +0530

    files added

---
+++ js/jspractice.js
@@ -0,0 +1,25 @@
+function getvalue(){
+
+
+    var listofnames=["mani", "kanna","yuva", "yugan"];
+    console.log("lengnth of array listofnames....",listofnames.length);
+    console.log(listofnames[0]);
+    console.log(listofnames[1]);
+    console.log(listofnames[2]);
+    console.table(listofnames);
+
+    for(var a=0;a<listofnames.length;a++){
+        console.log(listofnames[a]);
+    }
+    console.log("output from for OF");
+    for(var name of listofnames );
+    for(var a=0;a<listofnames.length;a++){
+        console.log(listofnames[a]);
+        if(a==2);
+        break;
+    }}
+    getvalue();
+
+    var listofcars={Name:"Shift",price:"20k",mdate:"2010"}
+    console.log(listofcars);
+    console.log(listofcars);
